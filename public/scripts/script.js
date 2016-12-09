2da85174c8fee7d52266ff92bb2e3e530a9ffb2b
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-08 21:16:32 -0600

    displaying last guess stats on dom

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,6 +13,28 @@ var playerTwo = new Player('Player Two');
 var playerThree = new Player('Player Three');
 var playerFour = new Player('Player Four');
 
+var displayGuessDetails = function(array){
+  console.log('in displayGuessDetails');
+  //clear pastGuess div
+  $('#pastGuess').html('');
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].difference === 'match') {
+      //add functionality to display that this player wins on dom, and button to restart game
+      alert(array[i].name + ' wins!');
+      console.log(array[i].name, "wins");
+    } else {
+      // display past guess details on dom
+      $('#pastGuess').append('<div class="playerGuessContainer col-sm-3"></div>');
+      $el = $('#pastGuess').children().last();
+      $el.append('<div class="playerGuess"></div>');
+      $guess = $el.children().last();
+      $guess.append('<p>'+ array[i].name +'</p>');
+      $guess.append('<p>Last guess: ' + array[i].guess + '</p>');
+      $guess.append('<p>' + array[i].difference + '</p>');
+    }
+  }
+}; // end displayGuessDetails
+
 var postMaxNum = function(num) {
   $.ajax({
     type: "POST",
@@ -34,6 +56,7 @@ var postInputs = function(playersArray) {
     url: '/postInputs',
     success: function(response) {
       console.log('postInputs ajax success. response: ', response);
+      displayGuessDetails(response);
     },
     error: function(){
       console.log('get max ajax error');
